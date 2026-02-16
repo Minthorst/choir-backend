bd77dd2bc05947963e9dab21436cae17eb949a88
lindhorst <lindhorst1990@gmail.com>
2026-02-16 19:57:53 +0100

    Initial commit

---
+++ src/main/java/me/choir_backend/ChoirBackendApplication.java
@@ -0,0 +1,13 @@
+package me.choir_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoirBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoirBackendApplication.class, args);
+	}
+
+}
